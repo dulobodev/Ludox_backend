0fc6fb12aad949a640c389af46bf7931d918f2ef
EDUARDO COSTA LOBO <eduardo.clobo@senacsp.edu.br>
2025-09-25 21:33:45 -0300

    atualizado envv

--- Backend/src/core/db.py
+++ Backend/src/core/db.py
@@ -1,11 +1,7 @@
 from pymongo import MongoClient
-
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-DB_NAME = os.getenv("DB_NAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-
-client = MongoClient(f"mongodb+srv://{DB_NAME}:{DB_PASSWORD}@ludox.6bhfhml.mongodb.net/?retryWrites=true&w=majority&appName=Ludox")
+MONGODB_URI = os.getenv("MONGODB_URI")
+client = MongoClient(MONGODB_URI)
