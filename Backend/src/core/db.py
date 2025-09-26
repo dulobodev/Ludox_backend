06970ee32d1a8e200fe9778a6a53ca7c4559b097
Du <duducostalobo10@gmail.com>
2025-09-26 10:47:38 -0300

    adicionado tratamento de erro na conexao

--- Backend/src/core/db.py
+++ Backend/src/core/db.py
@@ -4,4 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 MONGODB_URI = os.getenv("MONGODB_URI")
-client = MongoClient(MONGODB_URI)
+try:
+    client = MongoClient(MONGODB_URI)
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
